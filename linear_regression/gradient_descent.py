08987eecf685ba3eedbd4bd6f4330581264caf30
JaniKoivisto <koivisto.jani@live.fi>
2018-03-27 11:47:42 +0300

    Changes in MSE, plot costs and data

--- linear_regression/gradient_descent.py
+++ linear_regression/gradient_descent.py
@@ -1,38 +1,34 @@
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
-import csv
 
-def readData(filename):
-    xs = []
-    ys = []
- 
-    with open(filename) as csvDataFile:
-        csvReader = csv.reader(csvDataFile)
-        for row in csvReader:
-            xs.append(row[0])
-            ys.append(row[1])
- 
-    return np.asarray(xs, dtype=float), np.asarray(ys, dtype=float)
+def loadData(filename):
+    data = np.genfromtxt(filename, delimiter=',')
+    return data
 
-train_x,train_y = readData("train_data.csv")
 learning_rate = 0.01
-samples = len(train_x)
 epsilon = epsilon=1e-7
 
-X = tf.placeholder(tf.float32)
-Y = tf.placeholder(tf.float32)
+data = loadData("training2.csv")
+train_x = data[:,:2]
+N = train_x.shape[0]
+train_x = np.hstack((np.ones((train_x.shape[0], 1)), train_x))
 
-weigth = tf.Variable(0.0)
-bias = tf.Variable(0.0)
+train_y = data[:, 2].reshape(N,1)
+
+X = tf.placeholder(tf.float32, [N, train_x.shape[1]])
+Y = tf.placeholder(tf.float32, [N, 1])
+
+weigth = tf.Variable(tf.zeros([train_x.shape[1],train_y.shape[1]]), name="w")
+bias = tf.Variable(tf.zeros([1]), name="b")
 
 with tf.name_scope("linear_model") as scope:
     # W * X + b
-    model = tf.add(tf.multiply(weigth, X), bias)
+    model = tf.add(tf.matmul(X, weigth), bias)
 
 with tf.name_scope("cost") as scope:
     # MSE
-    cost = tf.reduce_sum(tf.pow(model-Y, 2))/(2 * samples)
+    cost = tf.reduce_mean(tf.squared_difference(model,Y))
 
 with tf.name_scope("train") as scope:
     # Gradient descent
@@ -44,23 +40,21 @@ with tf.Session() as sess:
     sess.run(init)
     training_cost = -np.inf
     previous_cost_value = np.inf
+    costs = []
     iteration = 0
     while np.abs(training_cost - previous_cost_value) >= epsilon:
         for (x,y) in zip(train_x,train_y):
-            sess.run(optimizer, {X: x, Y: y})
-            training_cost = previous_cost_value
-            previous_cost_value = sess.run(cost, feed_dict={X: train_x, Y: train_y})
+            sess.run(optimizer, {X: train_x, Y: train_y})
+            previous_cost_value = training_cost
+            training_cost = sess.run(cost, feed_dict={X: train_x, Y: train_y})
+            costs.append(training_cost)
             if (iteration + 1) % 100 == 0:
-                print("Iteration: ", (iteration + 1), "Training cost:", previous_cost_value, "weigth=", sess.run(weigth))
+                print("Iteration: ", (iteration + 1), "Training cost:", previous_cost_value, "Weigth=", sess.run(weigth))
             iteration += 1
-    training_cost = sess.run(cost, feed_dict={X: train_x, Y: train_y})
     print("Training completed!")
-    print("Iterations: ", (iteration), "Training cost:", training_cost, "weigth=", sess.run(weigth))
-
-    '''
-    plt.plot(train_x, train_y, 'ro', label='Observed data')
-    plt.plot(train_x, sess.run(weigth) * train_x + sess.run(b), label='Prediction')
-    plt.legend()
+    print("Iterations: ", (iteration), "Training cost:", costs[-1], "Weigth=", sess.run(weigth))
+    
+    plt.plot(costs)
+    plt.xlabel("# of iteration")
+    plt.ylabel("cost function value")
     plt.show()
-
-    '''
